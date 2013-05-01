e428b79cb7355b5e25f6c66ac2703b95948ff9fe
Alex Genco <alexgenco@gmail.com>
2013-04-30 20:00:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beatnode::Application.config.secret_key_base = '411195a3ea4903aa07bb19e90dd9af327a668064f3eb7e6b660788ec26871a8624d8867441cdc0bc610f789fedce6d3e4af8be6142e658f4ae74af71c3ba4d6f'
