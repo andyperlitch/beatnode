50df68a9a45a24811d735030a2d747b9a69480a9
Alex Genco <alexgenco@gmail.com>
2013-05-03 23:16:22 -0700

    use symbols instead of self

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,12 +1,12 @@
 class Node < Sequel::Model
   many_to_one :uploader, class: :User
 
-  many_to_many :parents, class: self,
+  many_to_many :parents, class: :Node,
     left_key:   :child_id,
     right_key:  :parent_id,
     join_table: :connections
 
-  many_to_many :children, class: self,
+  many_to_many :children, class: :Node,
     left_key:   :parent_id,
     right_key:  :child_id,
     join_table: :connections
